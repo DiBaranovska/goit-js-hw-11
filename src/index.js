60237b9444d5c5dbdb12ec6efa10e15a9bae7ecd
DiBaranovska <dmitrudi@gmail.com>
2023-03-31 17:55:10 +0300

    розмітка та запит на сервер

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import './css/styles.css';
-import Notiflix from 'notiflix';
+//import Notiflix from 'notiflix';
+import axios from 'axios';
+const BASE_URL = "https://pixabay.com/api/";
+const API_KEY = "34925796-aa77653a24e3240cce9cedfc1";
+
+
+   /* async function getImages() {
+  try {
+      const response = await axios.get(`${BASE_URL}` ? key = " + "`${API_KEY}` + encodeURIComponent('yellow+flower'));
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+};*/
